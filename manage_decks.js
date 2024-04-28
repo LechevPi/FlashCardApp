749de0962565be4080995d06fab906e920beda78
Pierre Lechevallier <pierre.lechevallier@eawag.ch>
2024-04-28 10:57:56 +1000

    add deck function added

--- manage_decks.js
+++ manage_decks.js
@@ -9,16 +9,59 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const availableDecks = ref(database, "decks")
 
+const availableDeckContainer = document.getElementById("available-decks-container")
+
 /* Handling click on add buttons */
 
 const addButton = document.getElementById("add-new-deck-button")
 
 addButton.addEventListener("click", function() {
-    console.log("we are here")
     let deckName = prompt("Enter the deck name: ", "")
 
     if (deckName != "") {
         push(availableDecks, deckName)
     }
 
+    resetAvailableDeckContainer()
+
 })
+
+onValue(availableDecks, function(snapshot) {
+
+    if (snapshot.exists()) {
+
+        let deckArray = Object.entries(snapshot.val())
+
+        resetAvailableDeckContainer()
+
+        for (let i=0; i<deckArray.length; i++) {
+            let deckId = deckArray[i][0]
+            let deckName = deckArray[i][1]
+            appendDeckToAvailableDeckContainer(deckId, deckName)
+        
+        }
+
+    } else {
+        availableDeckContainer.innerHTML = "<p>Nothing in the list ...</p>"
+    }
+})
+
+function appendDeckToAvailableDeckContainer(id, name) {
+
+    let newEl = document.createElement("p")
+    newEl.textContent = name
+
+    newEl.addEventListener("click", function() {
+        deleteDeckFromDatabase(id)
+    })
+
+    availableDeckContainer.append(newEl)
+}
+
+function resetAvailableDeckContainer() {
+    availableDeckContainer.innerHTML = ""
+}
+
+function deleteDeckFromDatabase(elementId) {
+    remove(ref(database, `decks/${elementId}`))
+}
