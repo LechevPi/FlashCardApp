d0f852a51a029573c9bc8f86d871c471f5ce19d4
Pierre Lechevallier <pierre.lechevallier@eawag.ch>
2024-04-30 15:23:58 +1000

    delete deck functionality added

--- manage_decks.js
+++ manage_decks.js
@@ -8,23 +8,21 @@ const appSettings = {
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const availableDecks = ref(database, "decks")
+const mainDoc = document.getElementsByTagName("main")[0]
 
-const availableDeckContainer = document.getElementById("available-decks-container")
 
 /* Handling click on add buttons */
 
 const addButton = document.getElementById("add-new-deck-button")
 
 addButton.addEventListener("click", function() {
-    let deckName = prompt("Enter the deck name: ", "")
 
-    if (deckName != "") {
-        push(availableDecks, deckName)
-    }
-
-    resetAvailableDeckContainer()
-
+    addDeckSubwindow.style.display = "block"
 })
+
+/* Adding the available decks live */
+
+const availableDeckContainer = document.getElementById("available-decks-container")
 
 onValue(availableDecks, function(snapshot) {
 
@@ -48,14 +46,60 @@ onValue(availableDecks, function(snapshot) {
 
 function appendDeckToAvailableDeckContainer(id, name) {
 
-    let newEl = document.createElement("p")
-    newEl.textContent = name
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("deck");
 
-    newEl.addEventListener("click", function() {
-        deleteDeckFromDatabase(id)
+    let deckName = document.createElement("p");
+    deckName.textContent = name;
+
+    let manageButton = document.createElement("button");
+    manageButton.textContent = "manage";
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "delete";
+
+    deleteButton.addEventListener("click", function() {
+        confirmDeckDeletion(id, name);
+    });
+
+    newDiv.appendChild(deckName);
+    newDiv.appendChild(manageButton);
+    newDiv.appendChild(deleteButton);
+
+    mainDoc.appendChild(newDiv);
+}
+
+function confirmDeckDeletion(deckId, deckName) {
+
+    let newDiv = document.createElement("div");
+    newDiv.style.display = "block";
+    newDiv.style.position = "absolute";
+    newDiv.style.border = "1px solid black";
+    newDiv.style.top = "1rem";
+    newDiv.style.backgroundColor = "aqua";
+
+    let newDivText = document.createElement("p");
+    newDivText.textContent = `Confirm the deletion of the deck ${deckName}`;
+
+    let deleteButon = document.createElement("button");
+    deleteButon.textContent = "delete";
+
+    let exitButon = document.createElement("button");
+    exitButon.textContent = "keep";
+
+    deleteButon.addEventListener("click", function() {
+        deleteDeckFromDatabase(deckId)
+        newDiv.style.display = "none";
+    })
+    exitButon.addEventListener("click", function() {
+        newDiv.style.display = "none";
     })
 
-    availableDeckContainer.append(newEl)
+    newDiv.appendChild(newDivText)
+    newDiv.appendChild(deleteButon)
+    newDiv.appendChild(exitButon)
+
+    availableDeckContainer.appendChild(newDiv)
 }
 
 function resetAvailableDeckContainer() {
@@ -64,4 +108,35 @@ function resetAvailableDeckContainer() {
 
 function deleteDeckFromDatabase(elementId) {
     remove(ref(database, `decks/${elementId}`))
+}
+
+/* Adding a new deck */
+
+const addDeckSubwindow = document.getElementById("add-deck-subwindow")
+const leaveAddDeckSubwindow = document.getElementById("leave-add-deck-subwindow")
+const inputFieldElement = document.getElementById("input-field")
+const createNewDeckButton = document.getElementById("create-new-deck-button")
+
+leaveAddDeckSubwindow.addEventListener("click", function () {
+    hideAddDeckSubwindow()
+})
+
+createNewDeckButton.addEventListener("click", function() {
+    let deckName = inputFieldElement.value
+
+    if (deckName != "") {
+        push(availableDecks, deckName)
+    }
+    
+    clearInputField()
+    hideAddDeckSubwindow()
+
+})
+
+function hideAddDeckSubwindow() {
+    addDeckSubwindow.style.display = "none"
+}
+
+function clearInputField() {
+    inputFieldElement.value = ""
 }
